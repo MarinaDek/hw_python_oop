f2e884b4c872d6783262ef11c2de8d6de0994b56
MarinaDek <dektyova-marina@yandex.ru>
2024-01-28 01:04:14 +0500

    My commit

--- homework.py
+++ homework.py
@@ -1,68 +1,77 @@
-class InfoMessage:
-    """Информационное сообщение о тренировке."""
-    pass
+#Калькулятор денег
+#
+# Калькулятор денег должен уметь:
+# Сохранять новую запись о расходах методом add_record()
+# Считать, сколько денег потрачено сегодня методом get_today_stats()
+# Определять, сколько ещё денег можно потратить сегодня в рублях, долларах или евро — метод get_today_cash_remained(currency)
+# Считать, сколько денег потрачено за последние 7 дней — метод get_week_stats()
+import datetime as dt
+
+class Record:
+    def __init__(self, amount, comment, date=None):
+        self.amount = amount
+        if date is None:
+            self.date = dt.date.today()
+        else:
+            self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
+        self.comment = comment
+
+class  Calculator:
+    def __init__(self, limit):
+        self.limit = limit
+        self.records = []
+
+    def add_record(self, record):
+        self.records.append(record)
+
+    def get_today_stats(self):
+        today = dt.date.today()
+        return sum(record.amount for record in self.records if record.date == today)
+
+    def get_week_stat(self):
+        today = dt.date.today()
+        date_week_ago = today - dt.timedelta(days=7)
+        return sum(record.amount for record in self.records if date_week_ago < record.date <= today)
+
+    def get_limit_today(self):
+        return self.limit - self.get_today_stats()
+
+class CaloriesCalculator(Calculator):
+    def get_calories_remained(self):
+        limit_today = self.get_limit_today()
+        if limit_today > 0:
+            return f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {limit_today} кКал'
+        else:
+            return 'Хватит есть!'
 
 
-class Training:
-    """Базовый класс тренировки."""
-
-    def __init__(self,
-                 action: int,
-                 duration: float,
-                 weight: float,
-                 ) -> None:
-        pass
-
-    def get_distance(self) -> float:
-        """Получить дистанцию в км."""
-        pass
-
-    def get_mean_speed(self) -> float:
-        """Получить среднюю скорость движения."""
-        pass
-
-    def get_spent_calories(self) -> float:
-        """Получить количество затраченных калорий."""
-        pass
-
-    def show_training_info(self) -> InfoMessage:
-        """Вернуть информационное сообщение о выполненной тренировке."""
-        pass
+class CashCalculator(Calculator):
+    USD_RATE = 89.00
+    EURO_RATE = 96.69
+    RUB_RATE = 1
+    def get_today_cash_remained(self, currency):
+        money = {"руб": self.RUB_RATE,
+                 "euro": self.EURO_RATE,
+                 "usd": self.USD_RATE}
+        limit_today = self.get_limit_today()
+        limit_today_curr = abs(round(limit_today / money[currency], 2))
+        if limit_today > 0:
+            return f"На сегодня осталось {limit_today_curr} {currency}"
+        elif limit_today == 0:
+            return 'Денег нет, держись'
+        else:
+            return f'Денег нет, держись: твой долг - {limit_today_curr} {currency}'
 
 
-class Running(Training):
-    """Тренировка: бег."""
-    pass
+# создадим калькулятор денег с дневным лимитом 1000
+cash_calculator = CashCalculator(1000)
 
+# дата в параметрах не указана,
+# так что по умолчанию к записи должна автоматически добавиться сегодняшняя дата
+cash_calculator.add_record(Record(amount=145, comment="кофе"))
+# и к этой записи тоже дата должна добавиться автоматически
+cash_calculator.add_record(Record(amount=300, comment="Серёге за обед"))
+# а тут пользователь указал дату, сохраняем её
+cash_calculator.add_record(Record(amount=3000, comment="бар в Танин др", date='28.01.2019'))
 
-class SportsWalking(Training):
-    """Тренировка: спортивная ходьба."""
-    pass
-
-
-class Swimming(Training):
-    """Тренировка: плавание."""
-    pass
-
-
-def read_package(workout_type: str, data: list) -> Training:
-    """Прочитать данные полученные от датчиков."""
-    pass
-
-
-def main(training: Training) -> None:
-    """Главная функция."""
-    pass
-
-
-if __name__ == '__main__':
-    packages = [
-        ('SWM', [720, 1, 80, 25, 40]),
-        ('RUN', [15000, 1, 75]),
-        ('WLK', [9000, 1, 75, 180]),
-    ]
-
-    for workout_type, data in packages:
-        training = read_package(workout_type, data)
-        main(training)
-
+print(cash_calculator.get_today_cash_remained("руб"))
